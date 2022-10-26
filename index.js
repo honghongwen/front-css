54b068776a919e5eb721996280ad91a019c7420f
fengwen <983393186@qq.com>
2022-10-26 17:40:16 +0800

    feat: login and regist page

---
+++ index.js
@@ -0,0 +1,42 @@
+let switchCtn = document.querySelector("#switch-cnt");
+let switchC1 = document.querySelector("#switch-c1");
+let switchC2 = document.querySelector("#switch-c2");
+let switchCicle = document.querySelectorAll(".switch__cicle");
+let switchBtn = document.querySelectorAll(".switch-btn");
+let aContainer = document.querySelector("#a-container");
+let bContainer = document.querySelector("#b-container");
+let allButtons = document.querySelectorAll(".submit");
+
+let getButtons = (e) => e.preventDefault()
+
+let changeForm = (e) => {
+    switchCtn.classList.add("is-gx");
+    setTimeout(function(){
+        switchCtn.classList.remove("is-gx");
+    }, 1500)
+
+    switchCtn.classList.toggle("is-txr");
+    switchCicle[0].classList.toggle("is-txr");
+    switchCicle[1].classList.toggle("is-txr");
+
+    switchC1.classList.toggle("is-hidden");
+    switchC2.classList.toggle("is-hidden");
+    
+    aContainer.classList.toggle("is-txl");
+    bContainer.classList.toggle("is-txl");
+    bContainer.classList.toggle("is-z200");
+}
+
+
+
+let mainFun = (e) => {
+    for(var i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener("click", getButtons)
+    }
+    for(var i = 0; i < switchBtn.length; i++) {
+        switchBtn[i].addEventListener("click", changeForm);
+    }
+}
+
+window.addEventListener("click", mainFun);
+
